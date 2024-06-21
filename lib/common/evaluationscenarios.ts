445aee6a049836002699591d4e9ab073920fc2ac
lasse <lasse-winkel@t-online.de>
2024-06-21 15:43:09 +0200

    Update evaluation parameters

--- lib/common/evaluationscenarios.ts
+++ lib/common/evaluationscenarios.ts
@@ -11,19 +11,19 @@ const GOP_DEFAULTS = [2, 1, 0.5]
 const gopDefault = GOP_DEFAULTS[0]
 
 const GOP_THRESHOLDS = [
+	[0.99, 0.95],
 	[0.95, 0.9],
 	[0.9, 0.85],
-	[0.85, 0.8],
 ]
 const gopThresholds = GOP_THRESHOLDS[0]
 
-const PACKET_LOSS_SERVER_LINK = [0, 10, 20, 30, 35, 40]
+const PACKET_LOSS_SERVER_LINK = [0, 10, 20, 30]
 const packetLossServerLink = PACKET_LOSS_SERVER_LINK[0]
 
-const DELAYS_SERVER_LINK = [0, 20, 70, 200, 500]
+const DELAYS_SERVER_LINK = [0, 50, 200, 500]
 const delayServerLink = DELAYS_SERVER_LINK[1]
 
-const BANDWIDTH_CONSTRAINTS_SERVER_LINK = [1_000_000_000, 100_000_000, 10_000_000]
+const BANDWIDTH_CONSTRAINTS_SERVER_LINK = [100_000_000, 50_000_000, 10_000_000]
 const bandwidthConstraintServerLink = BANDWIDTH_CONSTRAINTS_SERVER_LINK[0]
 
 // Total number of experiments: 1 + 4 + 2 + 3 + 2 + 9 + 3 + 2 = 26. Each experiment 3 times -> 3 * 26 = 78.
